35003fb8f4b492dca2d6d1de679a5f4ed1d9ea65
UdaAwak <udaawak.gms@gmailcom>
2019-12-18 23:06:07 +0700

    Menyelesaikan Halaman Admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.templates.default')
+
+
+@section('content')
+    <h1>Halaman Dashboard Admin</h1>
+@endsection
--- resources/views/admin/templates/partials/header.blade.php
+++ resources/views/admin/templates/partials/header.blade.php
@@ -27,89 +27,43 @@
 <!-- Right navbar links -->
 <ul class="navbar-nav ml-auto">
     <!-- Messages Dropdown Menu -->
-    <li class="nav-item dropdown">
-    <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="far fa-comments"></i>
-        <span class="badge badge-danger navbar-badge">3</span>
-    </a>
-    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <a href="#" class="dropdown-item">
-        <!-- Message Start -->
-        <div class="media">
-            <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-            <div class="media-body">
-            <h3 class="dropdown-item-title">
-                Brad Diesel
-                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-            </h3>
-            <p class="text-sm">Call me whenever you can...</p>
-            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-            </div>
-        </div>
-        <!-- Message End -->
+
+    <li class="nav-item dropdown user-menu">
+        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+            <img src="{{ asset('asset/dist/img/user2-160x160.jpg') }}" class="user-image img-circle elevation-2" alt="User Image">
+            <span class="d-none d-md-inline">{{ auth()->user()->name }}</span>
         </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <!-- Message Start -->
-        <div class="media">
-            <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-            <div class="media-body">
-            <h3 class="dropdown-item-title">
-                John Pierce
-                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-            </h3>
-            <p class="text-sm">I got your message bro</p>
-            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-            </div>
-        </div>
-        <!-- Message End -->
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <!-- Message Start -->
-        <div class="media">
-            <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-            <div class="media-body">
-            <h3 class="dropdown-item-title">
-                Nora Silvester
-                <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-            </h3>
-            <p class="text-sm">The subject goes here</p>
-            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-            </div>
-        </div>
-        <!-- Message End -->
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-    </div>
+        <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <!-- User image -->
+            <li class="user-header bg-primary">
+            <img src="{{ asset('asset/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+
+            <p>
+                {{ auth()->user()->name }} - Web Developer
+                <small>The Owner</small>
+            </p>
+            </li>
+            <!-- Menu Body -->
+            
+            <!-- Menu Footer-->
+            <li class="user-footer">
+            <a href="#" class="btn btn-default btn-outline-primary btn-flat">Profile</a>
+            <a href="#"
+                    class="btn btn-default btn-outline-danger btn-flat float-right"
+                    data-toggle="modal" data-target="#modal-warning">
+                    Sign out
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+
+            </li>
+        </ul>
     </li>
-    <!-- Notifications Dropdown Menu -->
-    <li class="nav-item dropdown">
-    <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="far fa-bell"></i>
-        <span class="badge badge-warning navbar-badge">15</span>
-    </a>
-    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-item dropdown-header">15 Notifications</span>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <i class="fas fa-envelope mr-2"></i> 4 new messages
-        <span class="float-right text-muted text-sm">3 mins</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <i class="fas fa-users mr-2"></i> 8 friend requests
-        <span class="float-right text-muted text-sm">12 hours</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <i class="fas fa-file mr-2"></i> 3 new reports
-        <span class="float-right text-muted text-sm">2 days</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-    </div>
+
+
+
     </li>
     <li class="nav-item">
     <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
@@ -118,3 +72,28 @@
     </li>
 </ul>
 </nav>
+
+{{-- Modal-SwetAlert untuk logout --}}
+<div class="modal fade" id="modal-warning">
+    <div class="modal-dialog">
+    <div class="modal-content bg-warning">
+        <div class="modal-header">
+        <h4 class="modal-title">Peringatan</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="modal-body">
+        <p>Apakah kamu yakin ingin keluar.?</p>
+        </div>
+        <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Tidak</button>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();" 
+                    class="btn btn-outline-dark">Yakin</a>
+        </div>
+    </div>
+    <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
